370fa3d4d38c350c59bdbaefff44db0a8aa6584e
Hafez_001 <Hafez_001@DESKTOP-CQG29VN>
2023-12-30 10:25:46 +0330

    complete

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,29 @@
-import { Component } from '@angular/core';
+import { Component} from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { faker } from '@faker-js/faker';
 
 @Component({
   selector: 'app-root',
   standalone: true,
   imports: [CommonModule, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+
 })
+
 export class AppComponent {
-  title = 'typing';
+  randomText : string = faker.lorem.sentences();
+  result :boolean = false;
+  inputText :string = '';
+
+  onChangeInput(text:string){
+    this.inputText = text;
+  }
+  compare(letterRandom : string,letterEnter:string) : string{
+    if(!letterEnter){
+      return 'pending';
+    }
+    return letterEnter === letterRandom ? 'correct' : 'incorrect';
+  }
 }
